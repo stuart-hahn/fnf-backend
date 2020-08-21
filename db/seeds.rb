2747614fc3ac6431f4e0828dea9a783e6197eb7c
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2020-08-21 09:42:25 -0400

    add initial user seeds and put has_secure_password into user model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(name: "stuart", username: "slump", password: "test")
+User.create(name: "anthony", username: "antcap24", password: "test")
